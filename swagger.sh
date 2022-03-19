20d7bfad835015b22115e47bcef8e9149442e8b0
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-03-19 15:37:53 +0200

    add swagger codegen

---
+++ swagger.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java -jar swagger-codegen-cli.jar generate \
+  -i swagger.yaml \
+  -l typescript-angular \
+  -o ${PWD}/outputs/codeGen
