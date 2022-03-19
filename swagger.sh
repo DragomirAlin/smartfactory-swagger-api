8e02831602cefe356e4e2da4129bb93aaa06788a
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-03-19 15:53:45 +0200

    update swagger

--- swagger.sh
+++ swagger.sh
@@ -3,4 +3,4 @@
 /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java -jar swagger-codegen-cli.jar generate \
   -i swagger.yaml \
   -l typescript-angular \
-  -o ${PWD}/outputs/codeGen
+  -o ${PWD}/outputs/codegen
